a1492ceb20a23c73a885fc82da1dd0651f51d433
Leo79Jp <leojparodi@gmail.com>
2025-04-23 14:10:57 -0300

    Modifique la linea 9 y tenia razon!

--- Juego.js
+++ Juego.js
@@ -6,16 +6,16 @@ const opciones = [
 {opcion: 'tijera'} 
 ]
 let opcionElegida = prompt(`Ingrese una opcion ' piedra, papel o tijera ': `)
-let opcionRandom = opciones[Math.floor(Math.random() * opciones.length)];
+let opcionRandom = opciones[Math.floor(Math.random() * opciones.length)].opcion;
 const jugar = (OE, OR) =>{
 
-    if (OE == OR.opcion){
+    if (OE == OR){
         console.log(`Ambos eligieron '${OE}', es un empate!! `);
     }else{
         console.log(`Opcion elegida por el Usuario es: ${opcionElegida}`);
-        console.log(`opcion elegida por el Ordenador es: ${OR.opcion}`);
+        console.log(`opcion elegida por el Ordenador es: ${OR}`);
         console.log('---------------------------------------------------');
-        if ((OE === 'piedra' && OR.opcion === 'tijera') || (OE === 'papel' && OR.opcion === 'piedra') || (OE === 'tijera' && OR.opcion === 'papel')){
+        if ((OE === 'piedra' && OR === 'tijera') || (OE === 'papel' && OR === 'piedra') || (OE === 'tijera' && OR === 'papel')){
             console.log(`El Usuario a Ganado!!🎉🎉🎉`);
         }else{
             console.log('El usuario a perdido!!😒');
